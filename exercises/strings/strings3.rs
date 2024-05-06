dd446552d52b196ed1b71f32ff7761dc309f896e
Mathias Bischoff <mathiasbischo@gmail.com>
2024-05-06 21:04:51 +0200

    more exercises

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,23 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
+    let chars_to_trim: &[char] = &[' '];
+    String::from(input.trim_matches(chars_to_trim))
+
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut new_string = String::from(input);
+    new_string.push_str(" world!");
+    new_string
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let string = String::from(input);
+    let new_string = string.replace("cars", "balloons");
+    new_string
 }
 
 #[cfg(test)]
